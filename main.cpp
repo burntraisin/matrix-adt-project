8a2013f11d93bcc5fdde5585c2cf3051c8294f40
burntraisin <rbkehan@gmail.com>
2024-10-03 11:09:09 -0400

    Implement switch statement and file output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -16,7 +17,7 @@ void calc_sum(Matrix m1, Matrix m2, Matrix &sum);
 void calc_diff(Matrix m1, Matrix m2, Matrix &diff);
 void scalar_mult(int k, Matrix m, Matrix &k_m);
 void calc_prod(Matrix m1, Matrix m2, Matrix &prod);
-void calc_inv(Matrix m, double &m_inv);
+void calc_inv(Matrix m, Matrix &m_inv);
 
 int main() {
     int b = 0, userChoice = 0;
@@ -27,38 +28,88 @@ int main() {
     get_matrix("m2", m2);
 
     do {
-
+        cout << "Enter a number to perform one of the following on the two matrices:" << '\n' << "1. Sum" << '\n' << "2. Difference" << '\n' << "3. Multiply by a scalar" << '\n' << "4. Product" << '\n' << "5. Inverse" << '\n' << "0. Quit" << endl;
+        cin >> userChoice;
+        switch (userChoice) {
+            case 0:
+                break;
+            case 1:
+                calc_sum(m1, m2, sum);
+                // Print sum to console
+                break;
+            case 2:
+                calc_diff(m1, m2, diff);
+                // Print diff to console
+                break;
+            case 3:
+                get_scalar(b);
+                scalar_mult(b, m1, k_m);
+                // Print k_m to console
+                break;
+            case 4:
+                calc_prod(m1, m2, prod);
+                // Print prod to console
+                break;
+            case 5: 
+                calc_inv(m1, m_inv);
+                // Print m_inv to console
+                break;
+            default:
+                break;
+        }
     } while (userChoice != 0);
 
+    outfile.open("output.txt", ios::out);
+    print_matrix(outfile, "First matrix", m1);
+    print_matrix(outfile, "Second matrix", m2);
+    print_matrix(outfile, "Sum", sum);
+    print_matrix(outfile, "Difference", diff);
+    print_matrix(outfile, "Scalar Multiplication", k_m);
+    print_matrix(outfile, "Product", prod);
+    print_matrix(outfile, "Inverse", m_inv);
+    outfile.close();
     return 0;
 }
 
 void get_matrix(string name, Matrix &m) {
-    name = m.name;
-    cout << "Enter a value for element a_11 of the matrix: ";
+    m.name = name;
+    cout << "Enter a value for element a_11 of the matrix " << m.name << ": ";
     cin >> m.a11;
-    cout << "Enter a value for element a_12 of the matrix: ";
+    cout << "Enter a value for element a_12 of the matrix " << m.name << ": ";
     cin >> m.a12;
-    cout << "Enter a value for element a_21 of the matrix: ";
+    cout << "Enter a value for element a_21 of the matrix " << m.name << ": ";
     cin >> m.a21;
-    cout << "Enter a value for element a_22 of the matrix: ";
+    cout << "Enter a value for element a_22 of the matrix " << m.name << ": ";
     cin >> m.a22;
 }
 
 void print_matrix(fstream &outfile, string name, Matrix m) {
-    outfile << name << '\n' << m << endl;
+    int nameLength = name.length();
+    string nameLengthSpaces;
+    nameLengthSpaces.assign(nameLength, ' ');
+
+    outfile << string(nameLength, ' ') << string(3, ' ') << setw(7) << m.a11 << setw(7) << m.a12 << endl;
+    outfile << name << " = " << endl;
+    outfile << string(nameLength, ' ') << string(3, ' ') << setw(7) << m.a21 << setw(7) << m.a22 << endl << endl;
 }
 
 void get_scalar(int &b) {
-
+    cout << "Enter a scalar: ";
+    cin >> b;
 }
 
 void calc_sum(Matrix m1, Matrix m2, Matrix &sum) {
-
+    sum.a11 = m1.a11 + m2.a11;
+    sum.a12 = m1.a12 + m2.a12;
+    sum.a21 = m1.a21 + m2.a21;
+    sum.a22 = m1.a22 + m2.a22;
 }
 
 void calc_diff(Matrix m1, Matrix m2, Matrix &diff) {
-
+    diff.a11 = m1.a11 - m2.a11;
+    diff.a12 = m1.a12 - m2.a12;
+    diff.a21 = m1.a21 - m2.a21;
+    diff.a22 = m1.a22 - m2.a22;
 }
 
 void scalar_mult(int k, Matrix m, Matrix &k_m) {

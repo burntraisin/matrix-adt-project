d347ed8884f99bae667cf70113304d498c3bcf10
burntraisin <rbkehan@gmail.com>
2024-10-03 23:39:16 -0400

    Final run

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-struct Matrix {
+struct Matrix
+{
     double a11, a12, a21, a22;
     string name;
 };
@@ -18,8 +19,10 @@ void calc_diff(Matrix m1, Matrix m2, Matrix &diff);
 void scalar_mult(int k, Matrix m, Matrix &k_m);
 void calc_prod(Matrix m1, Matrix m2, Matrix &prod);
 void calc_inv(Matrix m, Matrix &m_inv);
+void print_matrix_to_console(string name, Matrix m);
 
-int main() {
+int main()
+{
     int b = 0, userChoice = 0;
     Matrix m1, m2, sum, diff, k_m, prod, m_inv;
     fstream outfile;
@@ -27,32 +30,42 @@ int main() {
     get_matrix("m1", m1);
     get_matrix("m2", m2);
 
-    do {
-        cout << "Enter a number to perform one of the following on the two matrices:" << '\n' << "1. Sum" << '\n' << "2. Difference" << '\n' << "3. Multiply by a scalar" << '\n' << "4. Product" << '\n' << "5. Inverse" << '\n' << "0. Quit" << endl;
+    do
+    {
+        cout << "Enter a number to perform one of the following on the two matrices:" 
+        << '\n' << "1. Sum" 
+        << '\n' << "2. Difference" 
+        << '\n' << "3. Multiply by a scalar" 
+        << '\n' << "4. Product" 
+        << '\n' << "5. Inverse" 
+        << '\n' << "0. Quit" 
+        << endl;
+
         cin >> userChoice;
-        switch (userChoice) {
+        switch (userChoice)
+        {
             case 0:
                 break;
             case 1:
                 calc_sum(m1, m2, sum);
-                // Print sum to console
+                print_matrix_to_console("Sum", sum);
                 break;
             case 2:
                 calc_diff(m1, m2, diff);
-                // Print diff to console
+                print_matrix_to_console("Difference", diff);
                 break;
             case 3:
                 get_scalar(b);
                 scalar_mult(b, m1, k_m);
-                // Print k_m to console
+                print_matrix_to_console("Scalar Multiplication", k_m);
                 break;
             case 4:
                 calc_prod(m1, m2, prod);
-                // Print prod to console
+                print_matrix_to_console("Product", prod);
                 break;
             case 5: 
                 calc_inv(m1, m_inv);
-                // Print m_inv to console
+                print_matrix_to_console("Inverse", m_inv);
                 break;
             default:
                 break;
@@ -71,7 +84,8 @@ int main() {
     return 0;
 }
 
-void get_matrix(string name, Matrix &m) {
+void get_matrix(string name, Matrix &m)
+{
     m.name = name;
     cout << "Enter a value for element a_11 of the matrix " << m.name << ": ";
     cin >> m.a11;
@@ -83,43 +97,73 @@ void get_matrix(string name, Matrix &m) {
     cin >> m.a22;
 }
 
-void print_matrix(fstream &outfile, string name, Matrix m) {
+void print_matrix(fstream &outfile, string name, Matrix m)
+{
     int nameLength = name.length();
     string nameLengthSpaces;
     nameLengthSpaces.assign(nameLength, ' ');
 
-    outfile << string(nameLength, ' ') << string(3, ' ') << setw(7) << m.a11 << setw(7) << m.a12 << endl;
+    outfile << fixed << setprecision(2);
+    outfile << string(nameLength, ' ') << string(3, ' ') << setw(12) << m.a11 << setw(12) << m.a12 << endl;
     outfile << name << " = " << endl;
-    outfile << string(nameLength, ' ') << string(3, ' ') << setw(7) << m.a21 << setw(7) << m.a22 << endl << endl;
+    outfile << string(nameLength, ' ') << string(3, ' ') << setw(12) << m.a21 << setw(12) << m.a22 << endl << endl;
 }
 
-void get_scalar(int &b) {
+void get_scalar(int &b)
+{
     cout << "Enter a scalar: ";
     cin >> b;
 }
 
-void calc_sum(Matrix m1, Matrix m2, Matrix &sum) {
+void calc_sum(Matrix m1, Matrix m2, Matrix &sum)
+{
     sum.a11 = m1.a11 + m2.a11;
     sum.a12 = m1.a12 + m2.a12;
     sum.a21 = m1.a21 + m2.a21;
     sum.a22 = m1.a22 + m2.a22;
 }
 
-void calc_diff(Matrix m1, Matrix m2, Matrix &diff) {
+void calc_diff(Matrix m1, Matrix m2, Matrix &diff)
+{
     diff.a11 = m1.a11 - m2.a11;
     diff.a12 = m1.a12 - m2.a12;
     diff.a21 = m1.a21 - m2.a21;
     diff.a22 = m1.a22 - m2.a22;
 }
 
-void scalar_mult(int k, Matrix m, Matrix &k_m) {
-
+void scalar_mult(int k, Matrix m, Matrix &k_m)
+{
+    k_m.a11 = k * m.a11;
+    k_m.a12 = k * m.a12;
+    k_m.a21 = k * m.a21;
+    k_m.a22 = k * m.a22;
 }
 
-void calc_prod(Matrix m1, Matrix m2, Matrix &prod) {
-
+void calc_prod(Matrix m1, Matrix m2, Matrix &prod)
+{
+    prod.a11 = ((m1.a11 * m2.a11) + (m1.a12 * m2.a21));
+    prod.a12 = ((m1.a11 * m2.a12) + (m1.a12 * m2.a22));
+    prod.a21 = ((m1.a21 * m2.a11) + (m1.a22 * m2.a21));
+    prod.a22 = ((m1.a21 * m2.a12) + (m1.a22 * m2.a22));
 }
 
-void calc_inv(Matrix m, Matrix &m_inv) {
+void calc_inv(Matrix m, Matrix &m_inv)
+{
+    double detA = ((m.a11 * m.a22) - (m.a21 * m.a12));
+    m_inv.a11 = (m.a22 / detA);
+    m_inv.a12 = (-m.a12 / detA);
+    m_inv.a21 = (-m.a21 / detA);
+    m_inv.a22 = (m.a11 / detA);
+}
 
+void print_matrix_to_console(string name, Matrix m)
+{
+    int nameLength = name.length();
+    string nameLengthSpaces;
+    nameLengthSpaces.assign(nameLength, ' ');
+
+    cout << fixed << setprecision(2);
+    cout << string(nameLength, ' ') << string(3, ' ') << setw(12) << m.a11 << setw(12) << m.a12 << endl;
+    cout << name << " = " << endl;
+    cout << string(nameLength, ' ') << string(3, ' ') << setw(12) << m.a21 << setw(12) << m.a22 << endl << endl;
 }
